a04393e59a6fef2a232c4822a3963bf9a4705db5
Till Kolditz <Till.Kolditz@gmail.com>
2016-08-08 15:32:48 +0200

    changes for size of lineorder column experiment

--- tools/sample/bm.sh
+++ tools/sample/bm.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-for sf in 1; do
-	for type in ssbm-q01 ssbm-q01_eager; do
-		./${type} ../../database/sf-${sf} >${type}-sf-${sf}.out
-	done
+
+#for sf in `seq 1 10`; do
+#	for type in ssbm-q01 ssbm-q01_eager; do
+#		./${type} ../../database/sf-${sf} >${type}-sf-${sf}.out
+#	done
+#done
+
+echo "" >lineorder_size.out
+for sf in `seq 1 10`; do
+	./lineorder_size ../../database/sf-${sf} | tee -a lineorder_size.out
 done
 
