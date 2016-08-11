9b009928ef4215e3e3bc64de09f8d96469f91c9e
Till Kolditz <Till.Kolditz@gmail.com>
2016-08-11 13:13:40 +0200

    merge bm.sh

--- tools/sample/bm.sh
+++ tools/sample/bm.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-echo "" >bm.out
-echo "" >bm.err
-
-#for sf in `seq 1 10`; do
-for sf in 1; do
-	for type in ssbm-q01 ssbm-q01_eager ssbm-q01_lazy; do
-#		./${type} ../../database/sf-${sf} >${type}-sf-${sf}.out
-		./${type} ../../database/sf-${sf} 1>>bm.out 2>>bm.err
+#for sf in 1; do
+for type in ssbm-q01 ssbm-q01_eager ssbm-q01_lazy; do
+	FILEOUT="${type}.out"
+	FILEERR="${type}.err"
+	echo "" >${FILEOUT}
+	echo "" >${FILEERR}
+	for sf in `seq 1 10`; do
+		./${type} ../../database/sf-${sf} 1>>${FILEOUT} 2>>${FILEERR}
+#		./${type} ../../database/sf-${sf}
 	done
 done
 
