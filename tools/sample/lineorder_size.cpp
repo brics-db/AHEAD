ab462bc894a8f1c2445646ba46dc5c53fc119ded
Till Kolditz <Till.Kolditz@gmail.com>
2016-08-11 13:11:31 +0200

    converted code into a function now we test checkA(), decodeA(), and checkAndDecodeA() for restiny_t, resshort_t, and resint_t... yay!

--- tools/sample/lineorder_size.cpp
+++ tools/sample/lineorder_size.cpp
@@ -5,7 +5,131 @@
  * Created on 1. August 2016, 12:20
  */
 
+#include <type_traits>
+
 #include "ssbm.hpp"
+
+StopWatch sw1;
+const size_t NUM_OPS = 17;
+nanoseconds::rep opTimes[NUM_OPS];
+size_t batSizes[NUM_OPS];
+size_t batConsumptions[NUM_OPS];
+bool hasTwoTypes[NUM_OPS];
+boost::typeindex::type_index headTypes[NUM_OPS];
+boost::typeindex::type_index tailTypes[NUM_OPS];
+
+const size_t NUM_RUNS = 10;
+const size_t LEN_TIMES = 13;
+const size_t LEN_SIZES = 12;
+const size_t LEN_TYPES = 16;
+string emptyString;
+
+template<typename T>
+struct TypeSelector;
+
+template<>
+struct TypeSelector<tinyint_t> {
+    typedef tinyint_t base_t;
+    typedef tinyint_col_t col_t;
+    typedef restiny_t res_t;
+    typedef restiny_col_t res_col_t;
+
+    static const res_t A;
+    static const res_t A_INV;
+    static const res_t A_UNENC_MAX;
+    static const res_t A_UNENC_MAX_U;
+};
+
+const TypeSelector<tinyint_t>::res_t TypeSelector<tinyint_t>::A = ::A_TINY;
+const TypeSelector<tinyint_t>::res_t TypeSelector<tinyint_t>::A_INV = ::A_TINY_INV;
+const TypeSelector<tinyint_t>::res_t TypeSelector<tinyint_t>::A_UNENC_MAX = ::A_TINY_UNENC_MAX;
+const TypeSelector<tinyint_t>::res_t TypeSelector<tinyint_t>::A_UNENC_MAX_U = ::A_TINY_UNENC_MAX_U;
+
+template<>
+struct TypeSelector<shortint_t> {
+    typedef shortint_t base_t;
+    typedef shortint_col_t col_t;
+    typedef resshort_t res_t;
+    typedef resshort_col_t res_col_t;
+
+    static const res_t A;
+    static const res_t A_INV;
+    static const res_t A_UNENC_MAX;
+    static const res_t A_UNENC_MAX_U;
+};
+
+const TypeSelector<shortint_t>::res_t TypeSelector<shortint_t>::A = ::A_SHORT;
+const TypeSelector<shortint_t>::res_t TypeSelector<shortint_t>::A_INV = ::A_SHORT_INV;
+const TypeSelector<shortint_t>::res_t TypeSelector<shortint_t>::A_UNENC_MAX = ::A_SHORT_UNENC_MAX;
+const TypeSelector<shortint_t>::res_t TypeSelector<shortint_t>::A_UNENC_MAX_U = ::A_SHORT_UNENC_MAX_U;
+
+template<>
+struct TypeSelector<int_t> {
+    typedef int_t base_t;
+    typedef int_col_t col_t;
+    typedef resint_t res_t;
+    typedef resint_col_t res_col_t;
+
+    static const res_t A;
+    static const res_t A_INV;
+    static const res_t A_UNENC_MAX;
+    static const res_t A_UNENC_MAX_U;
+};
+
+const TypeSelector<int_t>::res_t TypeSelector<int_t>::A = ::A_INT;
+const TypeSelector<int_t>::res_t TypeSelector<int_t>::A_INV = ::A_INT_INV;
+const TypeSelector<int_t>::res_t TypeSelector<int_t>::A_UNENC_MAX = ::A_INT_UNENC_MAX;
+const TypeSelector<int_t>::res_t TypeSelector<int_t>::A_UNENC_MAX_U = ::A_INT_UNENC_MAX_U;
+
+template<typename BaseType>
+void runTable(const char* strTable, const char* strTableAN, const char* strColumn) {
+    size_t x = 0;
+    MEASURE_OP(sw1, x, batBc, new typename TypeSelector<BaseType>::col_t(strTable, strColumn));
+    MEASURE_OP(sw1, x, batBcAN, new typename TypeSelector<BaseType>::res_col_t(strTableAN, strColumn));
+    MEASURE_OP(sw1, x, batTcAN, v2::bat::ops::copy(batBcAN));
+    MEASURE_OP(sw1, x, batTcAN2, v2::bat::ops::copy(batTcAN));
+
+    cout << "\n" << strTable << '.' << strColumn << ":";
+
+    cout << "\n  name\t" << setw(LEN_TIMES) << "time [ns]" << '\t' << setw(LEN_SIZES) << "size [#]" << '\t' << setw(LEN_SIZES) << "consum [B]" << '\t' << setw(LEN_TYPES) << "type head" << '\t' << setw(LEN_TYPES) << "type tail";
+    for (size_t i = 0; i < x; ++i) {
+        cout << "\n  op" << setw(2) << i << "\t" << setw(LEN_TIMES) << hrc_duration(opTimes[i]) << "\t" << setw(LEN_SIZES) << batSizes[i] << "\t" << setw(LEN_SIZES) << batConsumptions[i] << "\t" << setw(LEN_TYPES) << headTypes[i].pretty_name() << "\t" << setw(LEN_TYPES) << (hasTwoTypes[i] ? tailTypes[i].pretty_name() : emptyString);
+    }
+    cout << "\n+-----+ ORDERKEY      +---------------+---------------+\n";
+    cout << "| num |         check |        decode |  check+decode |\n";
+    cout << "+-----+---------------+---------------+---------------+\n";
+
+    for (size_t i = 0; i < NUM_RUNS; ++i) {
+        cout << "|  " << setw(2) << i;
+
+        sw1.start();
+        auto result1 = v2::bat::ops::checkA(batTcAN, TypeSelector<BaseType>::A_INV, TypeSelector<BaseType>::A_UNENC_MAX_U);
+        sw1.stop();
+        cout << "   " << setw(LEN_TIMES) << sw1.duration() << flush;
+
+        delete result1;
+
+        sw1.start();
+        auto result2 = v2::bat::ops::decodeA<typename TypeSelector<BaseType>::base_t > (batTcAN, TypeSelector<BaseType>::A_INV, TypeSelector<BaseType>::A_UNENC_MAX_U);
+        sw1.stop();
+        cout << "   " << setw(LEN_TIMES) << sw1.duration() << flush;
+
+        delete result2;
+
+        sw1.start();
+        auto result3 = v2::bat::ops::checkAndDecodeA<typename TypeSelector<BaseType>::base_t > (batTcAN, TypeSelector<BaseType>::A_INV, TypeSelector<BaseType>::A_UNENC_MAX_U);
+        sw1.stop();
+        cout << "   " << setw(LEN_TIMES) << sw1.duration() << " |" << endl;
+
+        delete result3.first;
+        delete result3.second;
+    }
+    cout << "+-----+---------------+---------------+---------------+\n";
+    delete batTcAN2;
+    delete batTcAN;
+    delete batBcAN;
+    delete batBc;
+}
 
 int main(int argc, char** argv) {
     cout << "lineorder_size\n==============" << endl;
@@ -17,92 +141,31 @@ int main(int argc, char** argv) {
     string baseDir = p.remove_trailing_separator().generic_string();
     MetaRepositoryManager::init(baseDir.c_str());
 
-    TransactionManager* tm = TransactionManager::getInstance();
-    TransactionManager::Transaction* t;
-    StopWatch sw1, sw2;
-    string path;
-    size_t num;
 
-    t = tm->beginTransaction(true);
-    assert(t != nullptr);
-    sw2.start();
-    path = baseDir;
-    path += "/lineorder2"; // header file only contains the first column definition
-    num = t->load(path.c_str(), "lineorder");
-    sw2.stop();
-    cout << "File: " << path << "\n\tNumber of BUNs: " << num << "\n\tTime: " << sw2 << " ns." << endl;
-    tm->endTransaction(t);
+    loadTable(baseDir, "lineorder");
+    loadTable(baseDir, "lineorderAN");
 
-    t = tm->beginTransaction(true);
-    assert(t != nullptr);
-    sw2.start();
-    path = baseDir;
-    path += "/lineorder2AN"; // header file only contains the first column definition
-    num = t->load(path.c_str(), "lineorderAN");
-    sw2.stop();
-    cout << "File: " << path << "\n\tNumber of BUNs: " << num << "\n\tTime: " << sw2 << " ns." << endl;
-    tm->endTransaction(t);
-
-    const size_t NUM_OPS = 17;
-    nanoseconds::rep opTimes[NUM_OPS];
-    size_t batSizes[NUM_OPS];
-    size_t batConsumptions[NUM_OPS];
-    bool hasTwoTypes[NUM_OPS];
-    boost::typeindex::type_index headTypes[NUM_OPS];
-    boost::typeindex::type_index tailTypes[NUM_OPS];
-
-    const size_t LEN_TYPES = 13;
-    string emptyString;
-
+    // Lineorder:
     // orderkey|linenumber|custkey|partkey|suppkey|orderdate|orderpriority|shippriority|quantity|extendedprice|ordertotalprice|discount|revenue|supplycost|tax|commitdate|shipmode
-    // INTEGER|INTEGER|INTEGER|INTEGER|INTEGER|INTEGER|STRING|STRING|INTEGER|INTEGER|INTEGER|INTEGER|INTEGER|INTEGER|INTEGER|INTEGER|STRING
-    // RESINT|RESINT|RESINT|RESINT|RESINT|RESINT|STRING|STRING|RESINT|RESINT|RESINT|RESINT|RESINT|RESINT|RESINT|RESINT|STRING
+    // INTEGER|SHORTINT|INTEGER|INTEGER|INTEGER|INTEGER|STRING:15|CHAR|TINYINT|INTEGER|INTEGER|TINYINT|INTEGER|INTEGER|TINYINT|INTEGER|STRING:10
+    // RESINT|RESSHORT|RESINT|RESINT|RESINT|RESINT|STRING:15|CHAR|RESTINY|RESINT|RESINT|RESTINY|RESINT|RESINT|RESTINY|RESINT|STRING
 
-    size_t x = 0;
-    MEASURE_OP(sw1, x, batOKbcOrg, new int_col_t("lineorder", "orderkey"));
-    x++;
-    MEASURE_OP(sw1, x, batOKbcEnc, new resint_col_t("lineorderAN", "orderkey"));
-    x++;
-    MEASURE_OP(sw1, x, batOKtcEnc, v2::bat::ops::copy(batOKbcEnc));
-    x++;
-    MEASURE_OP(sw1, x, batOKtcEnc2, v2::bat::ops::copy(batOKtcEnc));
-    x++;
-    delete batOKtcEnc2;
-    cout << "\n\t  name\t" << setw(13) << "time [ns]\t" << setw(10) << "size [#]\t" << setw(10) << "consum [B]\t" << setw(LEN_TYPES) << "type head\t" << setw(LEN_TYPES) << "type tail";
-    for (size_t i = 0; i < x; ++i) {
-        cout << "\n\t  op" << setw(2) << i << "\t" << setw(13) << hrc_duration(opTimes[i]) << "\t" << setw(10) << batSizes[i] << "\t" << setw(10) << batConsumptions[i] << "\t" << setw(LEN_TYPES) << headTypes[i].pretty_name() << "\t" << setw(LEN_TYPES) << (hasTwoTypes[i] ? tailTypes[i].pretty_name() : emptyString);
-    }
-    cout << "\npeak RSS: " << getPeakRSS(size_enum_t::MB) << " MB.\n" << endl;
-    cout << " num |         check |        decode |  check+decode\n";
-    cout << "-----+---------------+---------------+--------------" << endl;
+    // RESINT
+    // ORDERKEY
+    runTable<int_t>("lineorder", "lineorderAN", "orderkey");
 
-    for (size_t i = 0; i < 1; ++i) {
-        sw1.start();
-        auto result1 = v2::bat::ops::checkA(batOKtcEnc, ::A_INT_INV, ::A_INT_UNENC_MAX_U);
-        sw1.stop();
-        cout << "  " << setw(2) << i << "   " << setw(13) << sw1.duration();
 
-        delete result1;
+    // RESSHORT
+    // LINENUMBER
+    runTable<shortint_t>("lineorder", "lineorderAN", "linenumber");
 
-        sw1.start();
-        auto result2 = v2::bat::ops::decodeA<int_t>(batOKtcEnc, ::A_INT_INV, ::A_INT_UNENC_MAX_U);
-        sw1.stop();
-        cout << "   " << setw(13) << sw1.duration();
 
-        delete result2;
 
-        sw1.start();
-        auto result3 = v2::bat::ops::checkAndDecodeA<int_t>(batOKtcEnc, ::A_INT_INV, ::A_INT_UNENC_MAX_U);
-        sw1.stop();
-        cout << "   " << setw(13) << sw1.duration() << '\n';
+    // RESTINY
+    // QUANTITY
+    runTable<tinyint_t>("lineorder", "lineorderAN", "quantity");
 
-        delete result3.first;
-        delete result3.second;
-    }
-
-    delete batOKbcOrg;
-    delete batOKbcEnc;
-    delete batOKtcEnc;
+    cout << "\npeak RSS: " << getPeakRSS(size_enum_t::MB) << " MB." << endl;
 
     return 0;
 }
