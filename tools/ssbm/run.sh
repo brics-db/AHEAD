e059ccb444a1865cd7cd8706d9a39d6a3f3b6673
Till Kolditz <Till.Kolditz@gmail.com>
2016-10-30 02:00:30 +0200

    fix for multiple queries (currently 1.1 and 1.2)

--- tools/ssbm/run.sh
+++ tools/ssbm/run.sh
@@ -2,7 +2,8 @@
 
 # Benchmarking
 BASE=ssbm-q
-IMPLEMENTED=(11 12 13)
+IMPLEMENTED=(11 12)
+
 for NUM in "${IMPLEMENTED[@]}"; do
 	BASE2=${BASE}${NUM}
 	for type in ${BASE2} ${BASE2}_eager ${BASE2}_lazy ${BASE2}_encoded; do
@@ -10,7 +11,8 @@ for NUM in "${IMPLEMENTED[@]}"; do
 		FILEERR="${type}.err"
 		echo "" >${FILEOUT}
 		echo "" >${FILEERR}
-		for sf in `seq 1 10`; do
+#		for sf in `seq 1 10`; do
+		for sf in 1; do
 			./${type} ../../database/sf-${sf} 1>>${FILEOUT} 2>>${FILEERR}
 #			./${type} ../../database/sf-${sf}
 		done
@@ -26,35 +28,35 @@ for NUM in "${IMPLEMENTED[@]}"; do
 	done
 done
 
-DIFF1=$(if [[ $(diff ssbm-q11.result ssbm-q11_eager.result | wc -l) -eq 0 ]]; then echo -n "OK"; else echo -n "FAIL"; fi)
-DIFF2=$(if [[ $(diff ssbm-q11.result ssbm-q11_lazy.result | wc -l) -eq 0 ]]; then echo -n "OK"; else echo -n "FAIL"; fi)
-DIFF3=$(if [[ $(diff ssbm-q11.result ssbm-q11_encoded.result | wc -l) -eq 0 ]]; then echo -n "OK"; else echo -n "FAIL"; fi)
-DIFF4=$(if [[ $(diff ssbm-q11_lazy.result ssbm-q11_eager.result | wc -l) -eq 0 ]]; then echo -n "OK"; else echo -n "FAIL"; fi)
-DIFF5=$(if [[ $(diff ssbm-q11_lazy.result ssbm-q11_encoded.result | wc -l) -eq 0 ]]; then echo -n "OK"; else echo -n "FAIL"; fi)
-DIFF6=$(if [[ $(diff ssbm-q11_eager.result ssbm-q11_encoded.result | wc -l) -eq 0 ]]; then echo -n "OK"; else echo -n "FAIL"; fi)
+EXITSTAT=0
+for NUM in "${IMPLEMENTED[@]}"; do
+    DIFF1=$(if [[ $(diff ssbm-q${NUM}.result ssbm-q${NUM}_eager.result | wc -l) -eq 0 ]]; then echo -n "OK"; else echo -n "FAIL"; fi)
+    DIFF2=$(if [[ $(diff ssbm-q${NUM}.result ssbm-q${NUM}_lazy.result | wc -l) -eq 0 ]]; then echo -n "OK"; else echo -n "FAIL"; fi)
+    DIFF3=$(if [[ $(diff ssbm-q${NUM}.result ssbm-q${NUM}_encoded.result | wc -l) -eq 0 ]]; then echo -n "OK"; else echo -n "FAIL"; fi)
+    DIFF4=$(if [[ $(diff ssbm-q${NUM}_lazy.result ssbm-q${NUM}_eager.result | wc -l) -eq 0 ]]; then echo -n "OK"; else echo -n "FAIL"; fi)
+    DIFF5=$(if [[ $(diff ssbm-q${NUM}_lazy.result ssbm-q${NUM}_encoded.result | wc -l) -eq 0 ]]; then echo -n "OK"; else echo -n "FAIL"; fi)
+    DIFF6=$(if [[ $(diff ssbm-q${NUM}_eager.result ssbm-q${NUM}_encoded.result | wc -l) -eq 0 ]]; then echo -n "OK"; else echo -n "FAIL"; fi)
 
-if [[ DIFF1 -eq "OK" ]] && [[ DIFF2 -eq "OK" ]] && [[ DIFF3 -eq "OK" ]] && [[ DIFF4 -eq "OK" ]] && [[ DIFF5 -eq "OK" ]] && [[ DIFF6 -eq "OK" ]]; then
-	echo "all OK"
-	exit 0;
-fi
-
-if [[ DIFF1 -eq "OK" ]]; then
-	if [[ DIFF2 -eq "OK" ]]; then
-		echo "All produce the same result"
-		exit 0
-	else
-		echo "Only ssbm-q11 and ssbm-q11_eager produce the same result"
-		exit 1
-	fi
-else
-	if [[ DIFF2 -eq "OK" ]]; then
-		echo "Only ssbm-q11 and ssbm-q11_lazy produce the same result"
-		exit 2
-	elif [[ DIFF3 -eq "OK" ]]; then
-		echo "Only ssbm-q11_lazy and ssbm-q11_eager produce the same result"
-		exit 3
-	fi
-fi
+    if [[ DIFF1 -eq "OK" ]] && [[ DIFF2 -eq "OK" ]] && [[ DIFF3 -eq "OK" ]] && [[ DIFF4 -eq "OK" ]] && [[ DIFF5 -eq "OK" ]] && [[ DIFF6 -eq "OK" ]]; then
+            echo "Q${NUM}: all OK"
+    elif [[ DIFF1 -eq "OK" ]]; then
+            if [[ DIFF2 -eq "OK" ]]; then
+                    echo "Q${NUM}: All produce the same result"
+            else
+                    echo "Only ssbm-q${NUM} and ssbm-q${NUM}_eager produce the same result"
+                    ++${EXITSTAT}
+            fi
+    else
+            if [[ DIFF2 -eq "OK" ]]; then
+                    echo "Only ssbm-q${NUM} and ssbm-q${NUM}_lazy produce the same result"
+                    ++${EXITSTAT}
+            elif [[ DIFF3 -eq "OK" ]]; then
+                    echo "Only ssbm-q${NUM}_lazy and ssbm-q${NUM}_eager produce the same result"
+                    ++${EXITSTAT}
+            fi
+    fi
+done
+exit ${EXITSTAT}
 
 #echo "" >lineorder_size.out; for sf in `seq 1 10`; do date +%T | tr -d '\n'; echo -n ": Scale Factor ${sf}... "; echo "Scale Factor ${sf}" >>lineorder_size.out; ./lineorder_size ../../database/sf-${sf} >>lineorder_size.out; date +%T | tr -d '\n'; echo ": Done!"; done
 
