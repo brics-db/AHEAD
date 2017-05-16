fb2174843ad54ed6544165dfcff6806a6713b501
Till Kolditz <Till.Kolditz@gmail.com>
2017-05-16 21:57:30 +0200

    add bootstrapping script to generate build directories to be used by eclipse projects update eclipse CDT project file (.cproject) to use updated build directories

---
+++ bootstrap.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env /bin/bash
+#
+# Copyright (c) 2017 Till Kolditz
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+# http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+BUILD_BASE_DIR=./build
+BUILD_DEBUG_DIR=${BUILD_BASE_DIR}/Debug
+BUILD_RELEASE_DIR=${BUILD_BASE_DIR}/Release
+
+[[ -e ${BUILD_DEBUG_DIR} ]] && rm -Rf ${BUILD_DEBUG_DIR}
+mkdir -p ${BUILD_DEBUG_DIR}
+[[ -e ${BUILD_RELEASE_DIR} ]] && rm -Rf ${BUILD_RELEASE_DIR}
+mkdir ${BUILD_RELEASE_DIR}
+
+pushd ${BUILD_DEBUG_DIR}
+cmake ../.. -DCMAKE_BUILD_TYPE=Debug
+popd
+
+pushd ${BUILD_RELEASE_DIR}
+cmake ../.. -DCMAKE_BUILD_TYPE=Release
+popd
