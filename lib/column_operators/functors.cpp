61c9e26e3e66ed32d31ed4025f425f2ea9ae2ca7
Till Kolditz <Till.Kolditz@gmail.com>
2017-05-24 22:41:54 +0200

    test how to implement functors for arbitrary arithmetic template concatenation

---
+++ lib/column_operators/functors.cpp
@@ -0,0 +1,60 @@
+// Copyright (c) 2017 Till Kolditz
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+// http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/* 
+ * File:   functors.cpp
+ * Author: Till Kolditz <till.kolditz@gmail.com>
+ *
+ * Created on 24-05-2017 14:39
+ */
+
+#include <iostream>
+
+#include "functors.hpp"
+
+/*
+ template<template<typename> typename Op, typename T>
+ constexpr
+ auto function(T a, T b) noexcept(noexcept(Op<void>()(a,b)))
+ -> decltype(Op<void>()(a, b)) {
+ return Op<void>()(a, b);
+ }
+ */
+
+template<typename Op, typename ... _Types>
+constexpr
+auto function(_Types ... args) noexcept(noexcept(Op()(std::forward<_Types>(args)...)))
+-> decltype(Op()(std::forward<_Types>(args)...)) {
+    return Op()(std::forward<_Types>(args)...);
+}
+
+int main(int argc, char ** argv) {
+    char* end;
+    ssize_t a(5), b(4), c(3), d(2);
+    if (argc > 1) {
+        a = strtoll(argv[1], &end, 10);
+    }
+    if (argc > 2) {
+        b = strtoll(argv[2], &end, 10);
+    }
+    if (argc > 3) {
+        c = strtoll(argv[3], &end, 10);
+    }
+    std::cout << "a+b=" << function<ahead::bat::ops::add<>>(a, b) << std::endl;
+    std::cout << "a-b=" << function<ahead::bat::ops::sub<>>(a, b) << std::endl;
+    std::cout << "a*b=" << function<ahead::bat::ops::mul<>>(a, b) << std::endl;
+    std::cout << "a/b=" << function<ahead::bat::ops::div<>>(a, b) << std::endl;
+    std::cout << "a+b+c=" << function<ahead::bat::ops::add<ahead::bat::ops::add<>>>(a, b, c) << std::endl;
+    std::cout << "a+(b*(c/d))=" << function<ahead::bat::ops::add<ahead::bat::ops::mul<ahead::bat::ops::div<>>> >(a, b, c, d) << std::endl;
+}
---
+++ lib/column_operators/functors.hpp
@@ -0,0 +1,120 @@
+// Copyright (c) 2017 Till Kolditz
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+// http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/* 
+ * File:   functors.hpp
+ * Author: Till Kolditz <till.kolditz@gmail.com>
+ *
+ * Created on 24-05-2017 14:04
+ */
+#ifndef INCLUDE_COLUMN_OPERATORS_FUNCTORS_HPP_
+#define INCLUDE_COLUMN_OPERATORS_FUNCTORS_HPP_
+
+namespace ahead {
+    namespace bat {
+        namespace ops {
+
+            template<typename _Op = void>
+            struct add {
+                template<typename _Tp, typename ... _Types>
+                constexpr
+                auto operator()(_Tp&& __t, _Types && ... args) const noexcept(noexcept(std::forward<_Tp>(__t) + _Op().operator()(args...)))
+                -> decltype(std::forward<_Tp>(__t) + _Op().operator()(args...))
+                {
+                    return std::forward<_Tp>(__t) + _Op().operator()(args...);
+                }
+            };
+
+            template<>
+            struct add<void> {
+                template<typename _Tp, typename _Up>
+                constexpr
+                auto operator()(_Tp&& __t, _Up&& __u) const noexcept(noexcept(std::forward<_Tp>(__t) + std::forward<_Up>(__u)))
+                -> decltype(std::forward<_Tp>(__t) + std::forward<_Up>(__u))
+                {
+                    return std::forward<_Tp>(__t) + std::forward<_Up>(__u);
+                }
+            };
+
+            template<typename _Op = void>
+            struct sub {
+                template<typename _Tp, typename ... _Types>
+                constexpr
+                auto operator()(_Tp&& __t, _Types && ... args) const noexcept(noexcept(std::forward<_Tp>(__t) - _Op().operator()(args...)))
+                -> decltype(std::forward<_Tp>(__t) - _Op().operator()(args...))
+                {
+                    return std::forward<_Tp>(__t) - _Op().operator()(args...);
+                }
+            };
+
+            template<>
+            struct sub<void> {
+                template<typename _Tp, typename _Up>
+                constexpr
+                auto operator()(_Tp&& __t, _Up&& __u) const noexcept(noexcept(std::forward<_Tp>(__t) - std::forward<_Up>(__u)))
+                -> decltype(std::forward<_Tp>(__t) - std::forward<_Up>(__u))
+                {
+                    return std::forward<_Tp>(__t) - std::forward<_Up>(__u);
+                }
+            };
+
+            template<typename _Op = void>
+            struct mul {
+                template<typename _Tp, typename ... _Types>
+                constexpr
+                auto operator()(_Tp&& __t, _Types && ... args) const noexcept(noexcept(std::forward<_Tp>(__t) * _Op().operator()(args...)))
+                -> decltype(std::forward<_Tp>(__t) * _Op().operator()(args...))
+                {
+                    return std::forward<_Tp>(__t) * _Op().operator()(args...);
+                }
+            };
+
+            template<>
+            struct mul<void> {
+                template<typename _Tp, typename _Up>
+                constexpr
+                auto operator()(_Tp&& __t, _Up&& __u) const noexcept(noexcept(std::forward<_Tp>(__t) * std::forward<_Up>(__u)))
+                -> decltype(std::forward<_Tp>(__t) * std::forward<_Up>(__u))
+                {
+                    return std::forward<_Tp>(__t) * std::forward<_Up>(__u);
+                }
+            };
+
+            template<typename _Op = void>
+            struct div {
+                template<typename _Tp, typename ... _Types>
+                constexpr
+                auto operator()(_Tp&& __t, _Types && ... args) const noexcept(noexcept(std::forward<_Tp>(__t) / _Op().operator()(args...)))
+                -> decltype(std::forward<_Tp>(__t) / _Op().operator()(args...))
+                {
+                    return std::forward<_Tp>(__t) / _Op().operator()(args...);
+                }
+            };
+
+            template<>
+            struct div<void> {
+                template<typename _Tp, typename _Up>
+                constexpr
+                auto operator()(_Tp&& __t, _Up&& __u) const noexcept(noexcept(std::forward<_Tp>(__t) / std::forward<_Up>(__u)))
+                -> decltype(std::forward<_Tp>(__t) / std::forward<_Up>(__u))
+                {
+                    return std::forward<_Tp>(__t) / std::forward<_Up>(__u);
+                }
+            };
+
+        }
+    }
+}
+
+#endif /* INCLUDE_COLUMN_OPERATORS_FUNCTORS_HPP_ */
