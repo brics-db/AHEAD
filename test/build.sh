1078cfded80c7367d6eed074f949052d1d7d5620
Till Kolditz <Till.Kolditz@gmail.com>
2017-08-09 14:31:45 +0200

    reset to -march=native

--- test/build.sh
+++ test/build.sh
@@ -2,13 +2,16 @@
 
 rm -Rf ./build
 
+#NPROCS=$NPROCS
+NPROCS=1
+
 c++ -std=c++14 -O3 -march=native -o createshufflemaskarrays createshufflemaskarrays.cpp &>createshufflemaskarrays.build &
-c++ -std=c++14 -O3 -mavx2 -o createshufflemaskarrays_avx2 createshufflemaskarrays_avx2.cpp &>createshufflemaskarrays_avx2.build &
-c++ -std=c++14 -O3 -march=native -DNPROCS=$(nproc) -o testshufflemask testshufflemask.cpp SSE.cpp ../lib/util/stopwatch.cpp -I../include -I../lib -fopenmp &>testshufflemask.build &
-c++ -std=c++14 -O2 -g3 -march=native -DNPROCS=$(nproc) -o testshufflemask_dbg testshufflemask.cpp SSE.cpp ../lib/util/stopwatch.cpp -I../include -I../lib -fopenmp &>testshufflemask_dbg.build &
+c++ -std=c++14 -O3 -march=native -o createshufflemaskarrays_avx2 createshufflemaskarrays_avx2.cpp &>createshufflemaskarrays_avx2.build &
+c++ -std=c++14 -O3 -march=native -DNPROCS=$NPROCS -o testshufflemask testshufflemask.cpp SSE.cpp ../lib/util/stopwatch.cpp -I../include -I../lib -fopenmp &>testshufflemask.build &
+c++ -std=c++14 -O2 -g3 -march=native -DNPROCS=$NPROCS -o testshufflemask_dbg testshufflemask.cpp SSE.cpp ../lib/util/stopwatch.cpp -I../include -I../lib -fopenmp &>testshufflemask_dbg.build &
 c++ -std=c++14 -O3 -march=native -o functors -I../include functors.cpp &>functors.build &
-c++ -std=c++14 -O3 -mavx2 -DNPROCS=$(nproc) -o testshufflemask_distributions testshufflemask_distributions.cpp SSE.cpp AVX2.cpp ../lib/util/stopwatch.cpp -I../include -I../lib -fopenmp &>testshufflemask_distributions.build &
-c++ -std=c++14 -O2 -g3 -mavx2 -DNPROCS=$(nproc) -o testshufflemask_distributions_dbg testshufflemask_distributions.cpp SSE.cpp AVX2.cpp ../lib/util/stopwatch.cpp -I../include -I../lib -fopenmp &>testshufflemask_distributions_dbg.build &
+c++ -std=c++14 -O3 -march=native -DNPROCS=$NPROCS -o testshufflemask_distributions testshufflemask_distributions.cpp SSE.cpp AVX2.cpp ../lib/util/stopwatch.cpp -I../include -I../lib -fopenmp &>testshufflemask_distributions.build &
+c++ -std=c++14 -O2 -g3 -march=native -DNPROCS=$NPROCS -o testshufflemask_distributions_dbg testshufflemask_distributions.cpp SSE.cpp AVX2.cpp ../lib/util/stopwatch.cpp -I../include -I../lib -fopenmp &>testshufflemask_distributions_dbg.build &
 
 wait $(jobs -p)
 
