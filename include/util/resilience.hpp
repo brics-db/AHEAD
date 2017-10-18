3d38d63230175831576622b82555ec1dc24a2b2f
Till Kolditz <Till.Kolditz@gmail.com>
2017-10-18 10:06:01 +0200

    for min bfw = 1 we can use 3 instead of 7 (1 bit less)

--- include/util/resilience.hpp
+++ include/util/resilience.hpp
@@ -62,12 +62,12 @@ namespace ahead {
         // static const constexpr std::array<A_t, 4> AtinyBFW = {Atiny[0], Atiny[2], Atiny[4], Atiny[7]};
         // static const constexpr std::array<restiny_t, 4> AtinyBFWInv = {AtinyInv[0], AtinyInv[2], AtinyInv[4], AtinyInv[7]};
         // sizeof(restiny_t) = 32-bit:
-        static const constexpr std::array<A_t, 7> AtinyBFW = {Atiny[0], Atiny[2], Atiny[4], Atiny[7], Atiny[10], Atiny[13], Atiny[15]};
-        static const constexpr std::array<restiny_t, 7> AtinyBFWInv = {AtinyInv[0], AtinyInv[2], AtinyInv[4], AtinyInv[7], AtinyInv[10], AtinyInv[13], AtinyInv[15]};
-        static const constexpr std::array<A_t, 6> AshortBFW = {Ashort[0], Ashort[2], Ashort[5], Ashort[8], Ashort[12], Ashort[15]};
-        static const constexpr std::array<resshort_t, 6> AshortBFWInv = {AshortInv[0], AshortInv[2], AshortInv[5], AshortInv[8], AshortInv[12], AshortInv[15]};
-        static const constexpr std::array<A_t, 5> AintBFW = {Aint[0], Aint[2], Aint[6], Aint[9], Aint[13]};
-        static const constexpr std::array<resint_t, 5> AintBFWInv = {AintInv[0], AintInv[2], AintInv[6], AintInv[9], AintInv[13]};
+        static const constexpr std::array<A_t, 7> AtinyBFW = {Atiny[0], Atiny[1], Atiny[4], Atiny[7], Atiny[10], Atiny[13], Atiny[15]};
+        static const constexpr std::array<restiny_t, 7> AtinyBFWInv = {AtinyInv[0], AtinyInv[1], AtinyInv[4], AtinyInv[7], AtinyInv[10], AtinyInv[13], AtinyInv[15]};
+        static const constexpr std::array<A_t, 6> AshortBFW = {Ashort[0], Ashort[1], Ashort[5], Ashort[8], Ashort[12], Ashort[15]};
+        static const constexpr std::array<resshort_t, 6> AshortBFWInv = {AshortInv[0], AshortInv[1], AshortInv[5], AshortInv[8], AshortInv[12], AshortInv[15]};
+        static const constexpr std::array<A_t, 5> AintBFW = {Aint[0], Aint[1], Aint[6], Aint[9], Aint[13]};
+        static const constexpr std::array<resint_t, 5> AintBFWInv = {AintInv[0], AintInv[1], AintInv[6], AintInv[9], AintInv[13]};
     };
 
     struct v2_anencoded_t {
