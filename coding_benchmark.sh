4e944a0e2148aef11d3e3c3e3ae07469da8bc6d5
Till Kolditz <till.kolditz@gmail.com>
2018-08-16 13:43:54 +0200

    use the SIGMOD2018reproducibility branch for coding_benchmark submodule initialize the coding_benchmark submodule recursively (it needs the segvcatch nested submodule)
    Signed-off-by: Till Kolditz <till.kolditz@gmail.com>

--- coding_benchmark.sh
+++ coding_benchmark.sh
@@ -8,8 +8,8 @@ source common.conf
 
 # For the reproducibility, use the submodule coding_benchmark
 if [[ -z "$(ls -A ${CB_SUBMODULE})" ]]; then
-    git submodule init "${CB_SUBMODULE}"
+    git submodule init --recursive "${CB_SUBMODULE}"
 fi
-git submodule update "${CB_SUBMODULE}"
+git submodule update --recursive "${CB_SUBMODULE}"
 
 pushd ${CB_SUBMODULE} && ./bootstrap.sh && pushd "${CB_BUILDDIR}/Release" && make -j$(nproc) && ./benchmark16 1> >(tee benchmark.out) 2> >(tee benchmark.err >&2) && popd && mv "${CB_BUILDDIR}/Release/benchmark.out" "${CB_BUILDDIR}/Release/benchmark.err" "../${AHEAD_PAPER_RESULTS_MB}" && popd && pushd "../${AHEAD_PAPER_RESULTS_MB}" && gnuplot plot_check_avx2.m && gnuplot plot_decode_avx2.m && gnuplot plot_encode_avx2.m && gnuplot plot_labels.m && popd
